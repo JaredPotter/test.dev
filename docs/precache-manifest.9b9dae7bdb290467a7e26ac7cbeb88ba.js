f0b5b8f4dbb04215c4f700fbec34e3e615ec600c
Larry512 <jaredpotter1@gmail.com>
2019-03-26 00:15:35 -0600

    init commit

---
+++ docs/precache-manifest.9b9dae7bdb290467a7e26ac7cbeb88ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "cc42fe10375454c0f1c8",
+    "url": "/static/js/main.b2e6c89a.chunk.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "cc42fe10375454c0f1c8",
+    "url": "/static/css/main.e273464b.chunk.css"
+  },
+  {
+    "revision": "38137d3d28b465e376d5228c30a04b6d",
+    "url": "/index.html"
+  }
+];
